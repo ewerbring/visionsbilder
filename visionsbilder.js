4089b32654881a67c718b75a595b6969e4fc3f93
Carl-Gustaf <ewerbring@gmail.com>
2019-05-02 10:09:28 -0400

    vision

---
+++ visionsbilder.js
@@ -0,0 +1,65 @@
+
+
+$( function() {
+   $( "#responcebox, .gubbe, .input_fields_wrap" ).draggable();
+ } );
+
+ $( function() {
+  $( "#commentbox" ).resizable();
+} );
+
+
+
+$('.gubbe').click(function() {
+
+   $('.input_fields_wrap').show();
+
+});
+
+$('.bgubbe').click(function() {
+
+   $('.b3').show();
+});
+$('.cgubbe').click(function() {
+
+   $('.b2').show();
+});
+
+$('.dgubbe').click(function() {
+
+   $('.b1').show();
+});
+$('.egubbe').click(function() {
+
+   $('.b4').show();
+});
+
+
+$('.egubbe').click(function() {
+
+   $('.abgubbe').show();
+
+});
+///textbox
+
+$(document).ready(function() {
+	var max_fields      = 10; //maximum input boxes allowed
+	var wrapper   		= $(".input_fields_wrap"); //Fields wrapper
+	var add_button      = $(".add_field_button"); //Add button ID
+
+	var x = 1; //initlal text box count
+	$(add_button).click(function(e){ //on add input button click
+		e.preventDefault();
+		if(x < max_fields){ //max input box allowed
+			x++; //text box increment
+			$(wrapper).append('<div><input class="comment" id="input" type="text" name="mytext[]"/><a href="#" class="remove_field"></a></div>'); //add input box
+		}
+	});
+
+	$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+		e.preventDefault(); $(this).parent('div').remove(); x--;
+	})
+});
+
+
+/////draggable
